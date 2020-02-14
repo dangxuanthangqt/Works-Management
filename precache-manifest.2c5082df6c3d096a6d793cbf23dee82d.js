538b6e2fade838332d3406abee93bd582a7364a0
dangxuanthang <dangxuanthangqt@gmail.com>
2020-02-14 11:31:47 +0700

    Updates

---
+++ precache-manifest.2c5082df6c3d096a6d793cbf23dee82d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e8ee1114e2a4bed6ae047c39abf724",
+    "url": "/Works-Management/index.html"
+  },
+  {
+    "revision": "1b30da3d5bb1d46fc0c5",
+    "url": "/Works-Management/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7af24354ea174b19079b",
+    "url": "/Works-Management/static/js/2.d261b6fe.chunk.js"
+  },
+  {
+    "revision": "57298b49bd6ed2c03c2216d2b1a7ee91",
+    "url": "/Works-Management/static/js/2.d261b6fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b30da3d5bb1d46fc0c5",
+    "url": "/Works-Management/static/js/main.e6af0ccb.chunk.js"
+  },
+  {
+    "revision": "715f762eddab8027b999",
+    "url": "/Works-Management/static/js/runtime-main.99e8560a.js"
+  }
+]);
